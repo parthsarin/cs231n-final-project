fa2a85bdad6602173408e0e1bc9403d326ef68bf
Parth Sarin <psarin@stanford.edu>
2024-05-12 11:16:40 -0700

    init data file

---
+++ data.py
@@ -0,0 +1,17 @@
+"""
+File: data.py
+"""
+
+
+def generate_mask(example):
+    """
+    Returns the 0/1 mask for where the license plate is with 1s on the license plate
+    """
+    pass
+
+
+def augment(example) -> list:
+    """
+    Augments the example in all the ways we were talking about
+    """
+    pass
