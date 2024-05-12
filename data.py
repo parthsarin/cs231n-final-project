d5406643ef2e673adcd318bce6ea823eeee48ac8
Parth Sarin <psarin@stanford.edu>
2024-05-12 12:07:35 -0700

    augmentation code

--- data.py
+++ data.py
@@ -1,17 +1,43 @@
 """
 File: data.py
 """
+import torch
+from torchvision.transforms import v2
+from torchvision import tv_tensors
+
+IMG_SIZE = (640, 640)
 
 
 def generate_mask(example):
     """
     Returns the 0/1 mask for where the license plate is with 1s on the license plate
     """
+    v2
     pass
 
 
-def augment(example) -> list:
+def augment(batch):
     """
-    Augments the example in all the ways we were talking about
+    Augments the batch
     """
-    pass
+    boxes = [
+        tv_tensors.BoundingBoxes(
+            example["objects"]["bbox"],
+            format="XYWH",
+            canvas_size=(example["height"], example["width"]),
+        )
+        for example in batch
+    ]
+
+    images = [example["image"] for example in batch]
+
+    transforms = v2.Compose(
+        [
+            v2.ToImage(),
+            v2.ToDtype(torch.uint8, scale=True),
+            v2.RandomResizedCrop(size=IMG_SIZE, antialias=True),
+            v2.ToDtype(torch.float32, scale=True),
+            # v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    return [transforms(image, box) for image, box in zip(images, boxes)]
--- train.py
+++ train.py
@@ -1,3 +1,43 @@
 """
 File: train.py
 """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import argparse
+import wandb
+
+wandb.init(project='cs231n-final-project')
+
+class BaselineModel(nn.Module):
+    def __init__(self, num_classes=2):
+        super(BaselineModel, self).__init__()
+        self.num_classes = num_classes
+
+
+        # assuming image size is 640x640
+        # predict probs for each pixel
+        self.m = nn.Sequential(
+            nn.Conv2d(3, 32, 3, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 3, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, 3, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(128, 256, 3, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(256 * 20 * 20, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, num_classes * 640 * 640)
+        )
+
+    def forward(self, x):
+        return F.sigmoid(self.m(x))
+
+
+if __name__ == '__main__':
