1faade8dde60d8d13b93d487c7d869c45fdf9d9d
Parth Sarin <psarin@stanford.edu>
2024-05-27 14:08:29 -0700

    train correctly

--- train.sh
+++ train.sh
@@ -3,10 +3,12 @@ python -m pip install -r requirements.txt
 
 export LD_LIBRARY_PATH=""
 
+python -m wandb login
+
 echo "Starting training..."
 nohup python train.py \
     --model baseline \
     --epochs 100 \
     --lr 0.05 \
-    --batch-size 64 \
+    --batch-size 256 \
     > train.log 2>&1 </dev/null &
