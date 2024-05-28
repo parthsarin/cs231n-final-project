40b8232cf3c0133fc6011c2a2f49c66bed53d2e0
Parth Sarin <psarin@stanford.edu>
2024-05-28 08:40:58 -0700

    fix train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,12 @@
 echo "Installing requirements..."
 python -m pip install -r requirements.txt
 
-export LD_LIBRARY_PATH=""
-
 python -m wandb login
 
 echo "Starting training..."
-nohup python train.py \
+python train.py \
     --model baseline \
     --epochs 100 \
     --lr 0.005 \
-    --lambda 8 \
-    --batch-size 512 \
-    > train.log 2>&1 </dev/null &
+    --lambda 5 \
+    --batch-size 512
